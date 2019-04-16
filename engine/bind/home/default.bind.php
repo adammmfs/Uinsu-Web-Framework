80fdb6e8913765ec4f95b270f1fd71e7d65360ea
Haxors Programming Club <44484256+haxorsprogramming@users.noreply.github.com>
2019-04-16 05:07:42 -0700

    Update default.bind.php

--- engine/bind/home/default.bind.php
+++ engine/bind/home/default.bind.php
@@ -4,7 +4,7 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title><?=SITENAME; ?> - Home</title>
+        <title><?=SITENAME; ?> - Home A</title>
 
         <!-- Fonts -->
        <link href="https://fonts.googleapis.com/css?family=Asap" rel="stylesheet">
